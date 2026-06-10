1675b9e3f4b98f851f4e8cc92838bb08a17d351d
chandani2710kalathiyainfotech <chandani.kalathiyainfotech@gmail.com>
2026-06-10 17:53:18 +0530

    second

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Header() {
+    return (
+        <>
+         <header class="nav-header">
+
+  <div class="logo">
+    MECH<span>ZONE</span>
+  </div>
+
+  <nav class="menu">
+    <a href="#">Home</a>
+    <a href="#">Machines</a>
+    <a href="#">Spare Parts</a>
+    <a href="#">Services</a>
+    <a href="#">Contact</a>
+  </nav>
+
+  <button class="cta">Get Quote</button>
+
+</header>
+        </>
+    )
+}
+
+export default Header;
