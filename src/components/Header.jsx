e5a03219bc7a2c8d5505d2687f67c5a44f0a799b
chandani2710kalathiyainfotech <chandani.kalathiyainfotech@gmail.com>
2026-06-11 09:35:23 +0530

    11_06_26

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 
 function Header() {
-    return (
-        <>
-         <header class="nav-header">
+  return (
+    <>
+      <header className="nav-header">
 
-  <div class="logo">
-    MECH<span>ZONE</span>
-  </div>
+        <div className="logo">
+          MECH<span>ZONE</span>
+        </div>
 
-  <nav class="menu">
-    <a href="#">Home</a>
-    <a href="#">Machines</a>
-    <a href="#">Spare Parts</a>
-    <a href="#">Services</a>
-    <a href="#">Contact</a>
-  </nav>
+        <nav className="menu">
+          <a href="#">Home</a>
+          <a href="#">Machines</a>
+          <a href="#">Spare Parts</a>
+          <a href="#">Services</a>
+          <a href="#">Contact</a>
+        </nav>
 
-  <button class="cta">Get Quote</button>
+        <button className="cta">Get Quote</button>
 
-</header>
-        </>
-    )
+      </header>
+      <h1 className="text-2xl text-amber-200">hfhfhf</h1>
+    </>
+  )
 }
 
 export default Header;
